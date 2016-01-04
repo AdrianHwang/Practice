7731d5b55840b5ba1031cd6c669a8863ad636481
AdrianHwang <boardsky@126.com>
2016-01-04 17:50:58 +0800

    improve todo text input

--- Redux-todomvc/components/Header.js
+++ Redux-todomvc/components/Header.js
@@ -2,11 +2,20 @@ import React, {PropTypes, Component} from 'react'
 import TodoTextInput from './TodoTextInput'
 
 class Header extends Component {
+    handleSave(text){
+        if(text.length !== 0){
+            console.log(text);
+        }
+    }
+
     render(){
         return (
             <header className="header">
                 <h1>Todos</h1>
-                <TodoTextInput />
+                <TodoTextInput 
+                newTodo
+                palceholder = "What needs to be done?"
+                onSave={this.handleSave.bind(this)}/>
             </header>
             )
     }
