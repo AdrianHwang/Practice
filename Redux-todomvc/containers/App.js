1d518f1dd7b544adc186e2750b9bcf2d43a40285
AdrianHwang <boardsky@126.com>
2016-01-04 17:05:54 +0800

    add header component

--- Redux-todomvc/containers/App.js
+++ Redux-todomvc/containers/App.js
@@ -1,9 +1,12 @@
 import React, { Component, PropTypes } from 'react'
+import Header from '../components/Header'
 
 class App extends Component {
     render() {
         return (
-                <div>hello world!</div>
+                <div>
+                    <Header />
+                </div>
             )
     }
 }
