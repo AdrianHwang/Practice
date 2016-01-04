ddfa2e5ebf0b99029cce43bdd38c2f1883c2ae0f
AdrianHwang <boardsky@126.com>
2016-01-04 15:19:50 +0800

    add react-redux

--- Redux-todomvc/index.js
+++ Redux-todomvc/index.js
@@ -1,9 +1,13 @@
 import 'babel-core/polyfill'
 import React from 'react'
 import { render } from 'react-dom'
+import { Provider } from 'react-redux'
 import 'todomvc-app-css/index.css'
 
+
 render(
-  <div>hello world! This is hello world program!</div>,
+  <Provider>
+    <div>hello world! This is hello world program!</div>
+  </Provider>,
   document.getElementById('root')
 )
