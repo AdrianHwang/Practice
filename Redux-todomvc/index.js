a1fcfd30efaf9f2deeb4da6ab321ebfdd1f4cdb0
AdrianHwang <boardsky@126.com>
2016-01-04 14:48:00 +0800

    init todo mvc

---
+++ Redux-todomvc/index.js
@@ -0,0 +1,9 @@
+import 'babel-core/polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import 'todomvc-app-css/index.css'
+
+render(
+  <div>hello world! This is hello world program!</div>,
+  document.getElementById('root')
+)
