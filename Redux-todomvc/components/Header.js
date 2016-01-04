3ebafa57216a5d65fbd7ac542919174cd671b726
AdrianHwang <boardsky@126.com>
2016-01-04 22:21:02 +0800

    improve Header component

--- Redux-todomvc/components/Header.js
+++ Redux-todomvc/components/Header.js
@@ -4,7 +4,7 @@ import TodoTextInput from './TodoTextInput'
 class Header extends Component {
     handleSave(text){
         if(text.length !== 0){
-            console.log(text);
+            this.props.addTodo(text)
         }
     }
 
@@ -13,12 +13,16 @@ class Header extends Component {
             <header className="header">
                 <h1>Todos</h1>
                 <TodoTextInput 
-                newTodo
-                placeholder = "What needs to be done?"
-                onSave={this.handleSave.bind(this)}/>
+                    newTodo
+                    placeholder = "What needs to be done?"
+                    onSave={this.handleSave.bind(this)}/>
             </header>
             )
     }
+}
+
+Header.propTypes = {
+    addTodo: PropTypes.func.isRequired
 }
 
 export default Header;
--- Redux-todomvc/containers/App.js
+++ Redux-todomvc/containers/App.js
@@ -1,14 +1,24 @@
 import React, { Component, PropTypes } from 'react'
 import Header from '../components/Header'
 
+
 class App extends Component {
     render() {
+        const {todos, actions} = this.props
+        let addTodo =  function(text){
+            console.log(text);
+        }
         return (
                 <div>
-                    <Header />
+                    <Header addTodo={addTodo}/>
                 </div>
             )
     }
 }
+
+App.propTypes = {
+
+}
+
 
 export default App;
