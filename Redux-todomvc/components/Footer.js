c4de9efebb7c912958e9de13ee3439849a842bfa
AdrianHwang <boardsky@126.com>
2016-01-06 16:41:17 +0800

    minor fix for ClearCompleted

--- Redux-todomvc/components/Footer.js
+++ Redux-todomvc/components/Footer.js
@@ -9,21 +9,25 @@ class Footer extends Component {
         const {todos, actions} = this.props
         const leftCount = todos.reduce((count, todo) =>
                 !todo.completed ? count + 1 : count, 0)
+        const completedCount =  todos.length - leftCount
 
         return (
             <footer className="footer">
                 <span className="todo-count">
                     <strong>{leftCount}</strong> Left
                 </span>
+
                 <ul className="filters">
                     <li><a href="#">All</a></li>
                     <li><a href="#">Active</a></li>
                     <li><a href="#">Completed</a></li>
                 </ul>
+
+                {!!completedCount && (
                 <button className="clear-completed"
                         onClick={actions.clearCompleted}>
                     Clear Completed
-                </button>
+                </button>)}
             </footer>
         )
 
