246f1c6baf2276a61b1a2f28170b1410fe0841eb
AdrianHwang <boardsky@126.com>
2016-01-06 16:16:07 +0800

    refactor the toggleAll codes

--- Redux-todomvc/components/MainSections.js
+++ Redux-todomvc/components/MainSections.js
@@ -6,15 +6,25 @@ class MainSections extends Component {
         super(props, context)
         this.state = {}
     }
+
+    renderToggleAll(completedCount){
+        const {todos, actions} = this.props
+        if(todos.length>0){
+            return (
+                <input type="checkbox"
+                       className="toggle-all"
+                       checked={completedCount === todos.length}
+                       onChange={actions.completeAll}/>
+            )
+        }
+    }
+
     render(){
         const {todos, actions } = this.props
         const completedCount = todos.reduce((count, todo) => todo.completed ? count + 1 : count, 0)
         return (
             <section className="main">
-                <input type="checkbox"
-                       className="toggle-all"
-                       checked={completedCount === todos.length}
-                       onChange={actions.completeAll}/>
+                {this.renderToggleAll(completedCount)}
                 <ul className="todo-list">
                     {todos.map(todo =>
                         <TodoItem key={todo.id} todo={todo} {...actions} />
